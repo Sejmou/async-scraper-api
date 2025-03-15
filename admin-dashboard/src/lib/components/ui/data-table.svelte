60001da5906e1e14a611fd783a62e72ace9c95b4
Sejmou <samo.kolter@gmail.com>
2025-03-15 13:56:37 +0100

    dashboard: better pagination for DataTable; TODO: fix memory issue when rendering large results

--- admin-dashboard/src/lib/components/ui/data-table.svelte
+++ admin-dashboard/src/lib/components/ui/data-table.svelte
@@ -7,25 +7,30 @@
 	} from '@tanstack/table-core';
 	import { createSvelteTable, FlexRender } from '$lib/components/ui/data-table';
 	import * as Table from '$lib/components/ui/table';
-	import { Button } from '$lib/components/ui/button';
-
-	// TODO: figure out how to use
 	import * as Pagination from '$lib/components/ui/pagination';
 
 	type DataTableProps<TData, TValue> = {
 		columns: ColumnDef<TData, TValue>[];
 		data: TData[];
-		paginationPageSize?: number;
+		paginationPageSize: number;
+		rowDescSingular: string;
+		rowDescPlural: string;
 	};
 
-	let { data, columns, paginationPageSize }: DataTableProps<TData, TValue> = $props();
-	if (paginationPageSize && paginationPageSize < 1) {
+	let {
+		data,
+		columns,
+		paginationPageSize = 10,
+		rowDescSingular = 'row',
+		rowDescPlural = 'rows'
+	}: DataTableProps<TData, TValue> = $props();
+	if (paginationPageSize < 1) {
 		throw new Error('paginationPageSize must be greater than 0');
 	}
 
 	let pagination = $state<PaginationState>({
 		pageIndex: 0,
-		pageSize: paginationPageSize ?? 10
+		pageSize: paginationPageSize
 	});
 
 	const table = createSvelteTable({
@@ -48,9 +53,37 @@
 		getCoreRowModel: getCoreRowModel(),
 		getPaginationRowModel: getPaginationRowModel()
 	});
+
+	function getPage() {
+		return table.getState().pagination.pageIndex + 1;
+	}
+
+	function setPage(newPage: number) {
+		table.setPageIndex(newPage - 1);
+	}
+
+	let pageIndex = $derived(table.getState().pagination.pageIndex);
+	let pageCount = $derived(table.getPageCount());
+	let rowCount = $derived(table.getCoreRowModel().rows.length);
+	let rowCountAndDesc = $derived(
+		`${rowCount} ` + (rowCount === 1 ? rowDescSingular : rowDescPlural)
+	);
+	$inspect({ rowCount, pageIndex, paginationPageSize, pageCount });
 </script>
 
 <div class="w-full">
+	<div class="w-full text-sm text-muted-foreground">
+		{#if pageCount > 1}
+			{rowCountAndDesc} (showing {rowDescPlural}
+			{pageIndex * paginationPageSize + 1} to {Math.min(
+				(pageIndex + 1) * paginationPageSize,
+				rowCount
+			)})
+		{:else}
+			{rowCountAndDesc}
+		{/if}
+	</div>
+
 	<div class="rounded-md border">
 		<Table.Root>
 			<Table.Header>
@@ -86,30 +119,34 @@
 			</Table.Body>
 		</Table.Root>
 	</div>
-	{#if table.getPageCount() > 1}
-		<!-- TODO: figure out how to use Pagination component -->
-		<div class="flex w-full items-center justify-between">
-			<div class="flex-1 text-sm text-muted-foreground">
-				Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
-			</div>
-			<div class="flex items-center justify-between space-x-2 py-4">
-				<Button
-					variant="outline"
-					size="sm"
-					onclick={() => table.previousPage()}
-					disabled={!table.getCanPreviousPage()}
-				>
-					Previous
-				</Button>
-				<Button
-					variant="outline"
-					size="sm"
-					onclick={() => table.nextPage()}
-					disabled={!table.getCanNextPage()}
-				>
-					Next
-				</Button>
-			</div>
+	{#if pageCount > 1}
+		<div class="mt-2 w-full">
+			<Pagination.Root bind:page={getPage, setPage} count={rowCount} perPage={paginationPageSize}>
+				{#snippet children({ pages, currentPage })}
+					<Pagination.Content>
+						<Pagination.Item>
+							<Pagination.PrevButton disabled={!table.getCanPreviousPage()} />
+						</Pagination.Item>
+						{#each pages as page (page.key)}
+							{#if page.type === 'ellipsis'}
+								<Pagination.Item>
+									<Pagination.Ellipsis />
+								</Pagination.Item>
+							{:else}
+								<!-- TODO: make work -> isVisible={currentPage === page.value} -->
+								<Pagination.Item>
+									<Pagination.Link {page} isActive={currentPage === page.value}>
+										{page.value}
+									</Pagination.Link>
+								</Pagination.Item>
+							{/if}
+						{/each}
+						<Pagination.Item>
+							<Pagination.NextButton disabled={!table.getCanNextPage()} />
+						</Pagination.Item>
+					</Pagination.Content>
+				{/snippet}
+			</Pagination.Root>
 		</div>
 	{/if}
 </div>
