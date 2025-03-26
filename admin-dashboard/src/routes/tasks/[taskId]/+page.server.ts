1b64a85973387b3564d76e61d3e664e91db51335
Sejmou <samo.kolter@gmail.com>
2025-03-26 04:00:50 +0100

    admin-dashboard: fix stupid bug in task page

--- admin-dashboard/src/routes/tasks/[taskId]/+page.server.ts
+++ admin-dashboard/src/routes/tasks/[taskId]/+page.server.ts
@@ -4,9 +4,15 @@ import { error } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
 import { addSubtaskProgressPromises } from '$lib/server/scraper-api/subtask-progress';
 
-export async function load() {
+export async function load({ params }) {
+	const id = parseInt(params.taskId, 10);
+	if (isNaN(id)) {
+		return error(400, {
+			message: 'Invalid task ID'
+		});
+	}
 	const taskWithSubTasksAndScrapers = await db.query.taskTbl.findFirst({
-		where: eq(taskTbl.id, 1),
+		where: eq(taskTbl.id, id),
 		with: {
 			subtasks: {
 				with: {
