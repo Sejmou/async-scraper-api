684108f09efd7292d436a2c16d2b27570ad95125
Sejmou <samo.kolter@gmail.com>
2025-05-07 16:02:59 +0200

    api: proper cursor-based pagination for queue_items endpoint (return items, next_cursor and total)

--- api-server/app/tasks/queue_items.py
+++ api-server/app/tasks/queue_items.py
@@ -6,12 +6,15 @@ type QueueType = Literal[
     "remaining_inputs", "successes", "failures", "inputs_without_output"
 ]
 
-_table_names = {
+_table_names: dict[QueueType, str] = {
     "remaining_inputs": "unique_queue_inputs",
     "successes": "queue_successes",
     "failures": "queue_failures",
     "inputs_without_output": "queue_inputs_without_output",
 }
+"""
+A dictionary mapping queue types to their corresponding SQLite table names.
+"""
 
 
 class TaskQueueItemManager:
@@ -32,7 +35,7 @@ class TaskQueueItemManager:
         last_id: int | None = None,
         limit: int = 10,
     ):
-        """Retrieve items from the item fetcher's SQLite database for the scraper task input queues.
+        """Retrieve items from the item fetcher's SQLite database for the scraper task input queues using cursor-based pagination (i.e. the last ID of the previous page is used to fetch the next page).
 
         Args:
             queue_type (str): The type of queue to fetch items from. Must be one of 'remaining_inputs', 'successes', 'failures', or 'inputs_without_outputs'.
@@ -40,7 +43,10 @@ class TaskQueueItemManager:
             limit (int, optional): The maximum number of items to fetch. Defaults to 10.
 
         Returns:
-            list: A list of dictionaries representing the fetched items. Each dictionary contains the keys 'id', 'data', and 'added_at'. Items are ordered by their ID in ascending order (i.e. oldest come first).
+            dict: A dictionary containing the fetched items and the ID of the next item to fetch.
+                - "items": A list of dictionaries representing the fetched items. Each dictionary contains the keys 'id', 'data', and 'added_at'. Items are ordered by their ID in ascending order (i.e. oldest come first).
+                - "next_cursor": The ID of the next item to fetch, or None if there are no more items.
+                - "total": The total number of items in the queue.
         """
 
         with sqlite3.connect(self.db_path) as conn:
@@ -58,7 +64,7 @@ class TaskQueueItemManager:
             )
             rows = cursor.fetchall()
 
-            rows = [
+            items = [
                 {
                     "id": row[0],
                     "data": row[1],
@@ -67,7 +73,24 @@ class TaskQueueItemManager:
                 for row in rows
             ]
 
-            return rows
+            last_id = items[-1]["id"] if items else None
+            if last_id:
+                next_id_tuple = cursor.execute(
+                    f"SELECT _id AS id FROM {_table_names[queue_type]} WHERE _id > ? ORDER BY id ASC LIMIT 1",
+                    (last_id,),
+                ).fetchone()
+                if next_id_tuple:
+                    next_id = next_id_tuple[0]
+                else:
+                    next_id = None
+            else:
+                next_id = None
+
+            total = cursor.execute(
+                f"SELECT COUNT(*) FROM {_table_names[queue_type]}",
+            ).fetchone()[0]
+
+            return {"items": items, "next_cursor": next_id, "total": total}
 
     def remove_queue_item(self, id: int, queue_type: QueueType):
         """Remove an item from the queue.
