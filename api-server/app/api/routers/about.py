53bc7692f42dff0e14d0455cbe2133a9ca5ba54e
Sejmou <samo.kolter@gmail.com>
2025-05-08 11:31:05 +0200

    api: add "online_since" field in data returned by "about" endpoint

--- api-server/app/api/routers/about.py
+++ api-server/app/api/routers/about.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+from datetime import datetime
 
 # TODO: update whenever something major changes in the API
 API_VERSION = "1.0"
+ONLINE_SINCE = datetime.now().isoformat()
 
 router = APIRouter(prefix="/about")
 
@@ -10,4 +12,5 @@ router = APIRouter(prefix="/about")
 def about():
     return {
         "version": API_VERSION,
+        "online_since": ONLINE_SINCE,
     }
