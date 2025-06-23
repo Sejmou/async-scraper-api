f2f63ce3222adca5188d7d59b1400396c45ecaa5
Sejmou <samo.kolter@gmail.com>
2025-06-23 23:44:15 +0200

    api: add retries + exponential backoff for reporting request metadata

--- api-server/app/utils/spotify_api/request_meta.py
+++ api-server/app/utils/spotify_api/request_meta.py
@@ -1,9 +1,15 @@
+from asyncio import sleep
 from datetime import datetime
 from pydantic import BaseModel
 import aiohttp
 
 from app.config import settings
 from app.utils.spotify_api.models import SpotifyAPICredentials
+
+MAX_RETRIES = 4
+"""
+The maximum number of retries for reporting request meta if the initial attempt fails for any reason.
+"""
 
 
 class SpotifyAPIRequestMeta(BaseModel):
@@ -15,18 +21,9 @@ class SpotifyAPIRequestMeta(BaseModel):
     credentials: SpotifyAPICredentials
 
 
-async def report_request_meta(
-    request_meta: SpotifyAPIRequestMeta,
+async def _make_request_meta_reporting_request(
+    meta: SpotifyAPIRequestMeta,
 ):
-    meta = SpotifyAPIRequestMeta(
-        url=request_meta.url,
-        status_code=request_meta.status_code,
-        sent_at=request_meta.sent_at,
-        received_at=request_meta.received_at,
-        ip=request_meta.ip,
-        credentials=request_meta.credentials,
-    )
-
     url = f"{settings.credentials_api_url}/spotify/request-meta"
     async with aiohttp.ClientSession() as session:
         try:
@@ -40,3 +37,27 @@ async def report_request_meta(
             raise RuntimeError(
                 f"Unexpected error while reporting request meta: {e}"
             ) from e
+
+
+async def report_request_meta(request_meta: SpotifyAPIRequestMeta):
+    meta = SpotifyAPIRequestMeta(
+        url=request_meta.url,
+        status_code=request_meta.status_code,
+        sent_at=request_meta.sent_at,
+        received_at=request_meta.received_at,
+        ip=request_meta.ip,
+        credentials=request_meta.credentials,
+    )
+    failed_attempts = 0
+    while failed_attempts < MAX_RETRIES:
+        try:
+            await _make_request_meta_reporting_request(meta)
+            return
+        except RuntimeError as e:
+            failed_attempts += 1
+            if failed_attempts >= MAX_RETRIES:
+                raise RuntimeError(
+                    f"Failed to report request meta after {MAX_RETRIES} attempts: {e}"
+                ) from e
+            # Exponential backoff before retrying
+            await sleep(2**failed_attempts)
